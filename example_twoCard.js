4e08755cec06615ad4cafa81edb3dd78d98b60eb
Suthas Ganeshathasan <suthas.gan@gmail.com>
2018-04-19 15:09:06 -0400

    added people info to card

--- example_twoCard.js
+++ example_twoCard.js
@@ -1,12 +1,27 @@
 const Card = (props) => {
   return(
     <div style={{margin: '1em'}}>
-      <img src="Http://placehold.it/75"/>
+      <img width= "75" src={props.avatar_url}/>
       <div style={{display: 'inline-block', marginLeft: 10 }}>
-        <div style={{fontSize: '1.25em', fontweight: 'bold' }}>Name here...</div>
-        <div>Company Name here ...</div>
+        <div style={{fontSize: '1.25em', fontweight: 'bold' }}>{props.name}</div>
+        <div>{props.company}</div>
         </div>
         </div>
   );
 };
-ReactDOM.render(<Card  />, mountNode);
+
+const CardList =  (props) => {
+  return (
+    <div>
+      <Card name="Suthas Ganeshathasan"
+      avatar_url="https://avatars0.githubusercontent.com/u/24394596?v=4"
+      company="Centennial College"/>
+      <Card name="Paul O'Shannessy"
+      avatar_url="https://avatars.githubusercontent.com/u/8445?v=3"
+      company="Facebook"/>
+    </div>
+
+  )
+}
+
+ReactDOM.render(<CardList />, mountNode);
