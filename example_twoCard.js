fa7a6646056b4bd9176aacefbd5f72e523d8f91b
Suthas Ganeshathasan <suthas.gan@gmail.com>
2018-04-19 14:26:32 -0400

    initial commit

---
+++ example_twoCard.js
@@ -0,0 +1,12 @@
+const Card = (props) => {
+  return(
+    <div>
+      <img src="Http://placehold.it/75"/>
+      <div>
+        <div>Name here...</div>
+        <div>Company Name here ...</div>
+        </div>
+        </div>
+  );
+};
+ReactDOM.render(<Card  />, mountNode);
