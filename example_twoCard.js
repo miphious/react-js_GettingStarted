5db3ebdc52c7e63fb1cd49a4510c0ffc0627a9b7
Suthas Ganeshathasan <suthas.gan@gmail.com>
2018-04-19 14:35:10 -0400

    added styles to card

--- example_twoCard.js
+++ example_twoCard.js
@@ -1,9 +1,9 @@
 const Card = (props) => {
   return(
-    <div>
+    <div style={{margin: '1em'}}>
       <img src="Http://placehold.it/75"/>
-      <div>
-        <div>Name here...</div>
+      <div style={{display: 'inline-block', marginLeft: 10 }}>
+        <div style={{fontSize: '1.25em', fontweight: 'bold' }}>Name here...</div>
         <div>Company Name here ...</div>
         </div>
         </div>
