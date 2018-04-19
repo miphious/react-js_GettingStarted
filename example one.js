e4c028dc235a37699455232f7d15e265fa2c8b38
Suthas Ganeshathasan <suthas.gan@gmail.com>
2018-04-19 13:07:21 -0400

    reusable components added

--- example one.js
+++ example one.js
@@ -1,15 +1,40 @@
 class Button extends React.Component {
   state = {counter:0};
 
-  handleClick = () => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1
-    }));
-  };
+  // handleClick = () => {
+  //   this.setState((prevState) => ({
+  //     counter: prevState.counter + 1
+  //   }));
+  // };
 
   render() {
     return (
-      <button onClick=(this.handleClick)> {this.state.counter}</button>
+      <button onClick=(this.handleClick)>
+      +1
+      </button>
+    );
+  }
+}
+
+const Result = (props) => {
+  return(
+    <div>...<div>
+  );
+};
+
+class App extends React.Component {
+  state = {counter:0};
+ incrementCounter = () => {
+   this.setState((prevState) => ({
+       counter: prevState.counter + 1
+     }));
+ }
+  render() {
+    return(
+      <div>
+      <Button />
+      <Result />
+      </div>
     );
   }
 }
